e297be5b07e429c8689ff308b5f0606f21fb5238
NENGO <44581262+Ally4@users.noreply.github.com>
2024-01-30 13:34:06 +0200

    Add files via upload

---
+++ (rido)Adminpage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Riddels.Pages
+{
+    public class AdminpageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
